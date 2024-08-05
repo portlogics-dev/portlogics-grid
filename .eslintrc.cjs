3c320e2effd03ee4a283b6cee6bead389fd9f8a5
portlogics-mingyukwak <mg.kwak@portlogics.com>
2024-08-05 16:57:02 +0900

    build: 개발환경 세팅

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,11 +8,55 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', "import", "@typescript-eslint", "prettier"],
+  settings: {
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"],
+    },
+    "import/resolver": {
+      typescript: {
+        project: ["apps/*/tsconfig.json", "packages/*/tsconfig.json"],
+      },
+    },
+    react: {
+      version: "detect",
+    },
+  },
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
+    ],
+    // import
+    "import/order": [
+      "error",
+      {
+        groups: [
+          ["builtin", "external"],
+          "internal",
+          ["parent", "sibling"],
+          "index",
+        ],
+        "newlines-between": "always",
+        pathGroups: [
+          {
+            pattern: "@/**",
+            group: "internal",
+            position: "after",
+          },
+        ],
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+      },
+    ],
+    // prettier
+    "prettier/prettier": [
+      "warn",
+      {
+        endOfLine: "auto",
+      },
     ],
   },
 }
